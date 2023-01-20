d371c617f0f5ddde2b691b0a012ab0a4add1a53f
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-01-20 14:24:10 +0100

    Esercizio Completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     
     data() {
      
         return {
      
-            
-     
+            title: 'Benvenuto Vue!',
+            mainLogo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png',
+            altImg: 'Logo Vue',
+            textWhite: 'text-white',
+
         }
     }
 
